779ff6057023315fc216e8d265c4acf5fa54599a
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2024-01-30 17:03:53 +0300

    android studio ynbary 2024

--- app/src/main/java/com/dsy/dsu/Hilt/PublicId/ModulePublicId.java
+++ app/src/main/java/com/dsy/dsu/Hilt/PublicId/ModulePublicId.java
@@ -39,7 +39,7 @@ public class ModulePublicId {
             ПубличныйID =
                     new Class_Generations_PUBLIC_CURRENT_ID().ПолучениеПубличногоТекущегоПользователяID(context);
             // TODO: 29.01.2024
-       ПубличныйID=96;
+       //////////////////ПубличныйID=96;
             Log.d(this.getClass().getName(), "\n" + " class " + Thread.currentThread().getStackTrace()[2].getClassName() + "\n" +
                     " metod " + Thread.currentThread().getStackTrace()[2].getMethodName() + "\n" +
                     " line " + Thread.currentThread().getStackTrace()[2].getLineNumber() + "\n"
