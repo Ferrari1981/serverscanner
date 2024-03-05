29eada612126e4d3303120529c29fad443593bc1
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2024-03-05 09:25:35 +0300

    android studio fefraly 2024

---
+++ Appdocuments/src/main/java/com/sous/appdocuments/GetAppdocuments.java
@@ -0,0 +1,11 @@
+package com.sous.appdocuments;
+
+import androidx.annotation.NonNull;
+
+public class GetAppdocuments {
+    public String appdocuments(@NonNull String s) {
+
+        System.out.println("fff" + s);
+        return s;
+    }
+}
--- mylibrary2/src/main/java/com/example/mylibrary2/myLibratu2.java
+++ mylibrary2/src/main/java/com/example/mylibrary2/myLibratu2.java
@@ -3,6 +3,7 @@ package com.example.mylibrary2;
 public class myLibratu2 {
 
     public   void getmain( ) {
+
         System.out.println("fff");
     }
 }
